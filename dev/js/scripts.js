8cfcf6df56a66061a9e9133a9b8e394ef8fcbd51
Blue <bk647@drexel.edu>
2022-10-20 11:25:18 -0400

    oh my god its actually working

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -29,84 +29,121 @@ closeBtn.addEventListener("click", () => {
 })
 
 
-
-function sideBars(){
-var tl = gsap.timeline();
-tl.from(".moveme", {
-    duration: 1,
-x: -875, 
-
-scrollTrigger: {
-    trigger: ".moveme",
-    markers: true,
-    start: "-1000%",
-    end: "10%",
-    scrub: true,
-    pin: false
+function movemes(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#best-friends-container", markers:false, scrub:true,end:"bottom center"}});
+    tl.from(".moveme",{duration:1,x:-800},"moveMeTogether")
+     .from(".moveme2",{duration:1,x:800},"moveMeTogether");
+    return tl;
 }
-});
+
+function fadeyGuys(){
+var tl = gsap.timeline({scrollTrigger:{trigger:"#callouts", markers: false, scrub:true,end:"bottom center"}});
+tl.from(".fadeBox", {duration: 1.5, opacity:0}, "fadeFriends")
+.from(".fadeBox2", {duration: 1.5, opacity:0}, "fadeFriends")
+.from(".callout-image", {duration: .5, opacity:0, rotate: 360, delay:.5}, "spinnyboys")
 return tl;
 }
 
-function sideBars2(){
-    var tl = gsap.timeline();
-    tl.from(".moveme2", {
-        duration: 1,
-    x: 875, 
-    
-    scrollTrigger: {
-        trigger: ".moveme2",
-        markers: true,
-        start: "-1000%",
-        end: "10%",
-        scrub: true,
-        pin: false
-    }
-    });
+function caaaards(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#article-layout", markers:true, scrub:true, end:"bottom bottom"}});
+    tl.from("#c1", {duration:.75, clipPath:"inset(0% 100% 0% 0%)"})
+    .from("#c2", {duration:.75, delay:.15, clipPath:"inset(0% 100% 0% 0%)"})
+    .from("#c3", {duration:.75,delay:.15, clipPath:"inset(0% 100% 0% 0%)"})
+    .from("#c4", {duration:.75,delay:.15, clipPath:"inset(0% 100% 0% 0%)"})
+    .from("#c5", {duration:.75,delay:.15, clipPath:"inset(0% 100% 0% 0%)"})
+    .from("#im1", {duration:.75, opacity:0},"-=2.5")
+    .from("#im2", {duration:.75, opacity:0},"-=2")
+    .from("#im3", {duration:.75, opacity:0},"-=1.5")
+    .from("#im4", {duration:.75, opacity:0},"-=1")
+    .from("#im5", {duration:.75, opacity:0},"-=.5");
     return tl;
-    }
+}
 
 
 
-    function fadeBox(){
-        var tl = gsap.timeline();
-        tl.from(".fadeBox", {
-            duration: 1,
-            alpha: 2,
+// 
+
+
+// function sideBars(){
+// var tl = gsap.timeline();
+// tl.from(".moveme", {
+//     duration: 1,
+// x: -875, 
+
+// scrollTrigger: {
+//     trigger: ".moveme",
+//     markers: true,
+//     start: "-100%",
+//     end: "-50%",
+//     scrub: true,
+//     pin: false
+// }
+// });
+// return tl;
+// }
+
+// function sideBars2(){
+//     var tl = gsap.timeline();
+//     tl.from(".moveme2", {
+//         duration: 1,
+//     x: 875, 
+    
+//     scrollTrigger: {
+//         trigger: ".moveme2",
+//         markers: true,
+//         start: "-100%",
+//         end: "-50%",
+//         scrub: true,
+//         pin: false
+//     }
+//     });
+//     return tl;
+//     }
+
+
+
+    // function fadeBox(){
+    //     var tl = gsap.timeline();
+    //     tl.from(".fadeBox", {
+    //         duration: 1,
+    //         alpha: 2,
         
-        scrollTrigger: {
-            trigger: ".fadeBox",
-            markers: true,
-            start: "-800%",
-            end: "-400",
-            scrub: true,
-            pin: false
-        }
-        });
-        return tl;
-        }
+    //     scrollTrigger: {
+    //         trigger: ".fadeBox",
+    //         markers: true,
+    //         start: "-100%",
+    //         end: "-20%",
+    //         scrub: true,
+    //         pin: false
+    //     }
+    //     });
+    //     return tl;
+    //     }
 
-    function fadeBox2(){
-        var tl = gsap.timeline();
-        tl.from(".fadeBox2", {
-            duration: 1,
-            alpha: 2,
+    // function fadeBox2(){
+    //     var tl = gsap.timeline();
+    //     tl.from(".fadeBox2", {
+    //         duration: 1,
+    //         alpha: 2,
         
-        scrollTrigger: {
-            trigger: ".fadeBox2",
-            markers: true,
-            start: "800%",
-            end: "400",
-            scrub: true,
-            pin: false
-        }
-        });
-        return tl;
-    }
+    //     scrollTrigger: {
+    //         trigger: ".fadeBox2",
+    //         markers: true,
+    //         start: "800%",
+    //         end: "400",
+    //         scrub: true,
+    //         pin: false
+    //     }
+    //     });
+    //     return tl;
+    // }
 
     
 var mainTL = gsap.timeline();
-mainTL.add(sideBars(), sideBars2(), fadeBox(), fadeBox2());
-
+mainTL.add(movemes())
+.add(fadeyGuys())
+.add(caaaards());
+// mainTL.add(sideBars(), sideBars2())
+// .add(fadeBox(), fadeBox2());
 
 
